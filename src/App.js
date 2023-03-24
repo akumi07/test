a76a3cdf54404c43880d05e071da057f91cd115d
akumi07 <akash9338144396@gmail.com>
2023-03-25 00:04:48 +0530

    test

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React from "react";
+import './App.css';
+import Header from './Header';
+import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from "./Checkout";
+
+function App() { 
+  return (
+    
+    <Router>
+
+    <div className="app">
+        <Switch>
+          {/* <Route path="/orders">
+            <Header />
+            <Orders />
+          </Route> */}
+          {/* <Route path="/login">
+            <Login />
+          </Route> */}
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
+          </Route>
+          {/* <Route path="/payment">
+            <Header />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
+          </Route> */}
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+         
+    
+    
+          
+               
+            
+              
+            
+            
+      
+      
+    
+    
+  
+    
+    
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBgkedJFuuz5QhMTBjwjw7cPg621_N3kRw",
+    authDomain: "clone-262fb.firebaseapp.com",
+    projectId: "clone-262fb",
+    storageBucket: "clone-262fb.appspot.com",
+    messagingSenderId: "196571249639",
+    appId: "1:196571249639:web:ee8f2e9e71c0105b565b42",
+    measurementId: "G-KJK89NWRT5"
+  };
